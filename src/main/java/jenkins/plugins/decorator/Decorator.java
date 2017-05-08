270c1f5ea20016d74bc194e48d7727bae4732f7e
Tom Huybrechts <tom.huybrechts@gmail.com>
2017-05-08 11:19:48 +0200

    first commit

---
+++ src/main/java/jenkins/plugins/decorator/Decorator.java
@@ -0,0 +1,25 @@
+/** PageMarkup: An HTML page markup plugin for Hudson
+ *  Copyright (c) 2008 Sandia Corporation.
+ *  All rights resserved.
+ *
+ * PageMarkup is part of the FAST software project, see
+ *   https://software.sandia.gov/trac/fast
+ *
+ * This software is distributed under the BSD License; for the full text
+ * of the license, see the LICENSE.txt file included with this
+ * distribution.  Under the terms of Contract DE-AC04-94AL85000, there is
+ * a non-exclusive license for use of this work by or on behalf of the
+ * U.S. Government. 
+ *
+ * Created by John D. Siirola <jdsiiro@sandia.gov>
+ * Sandia National Laboratories
+ * 4 June 2009
+ */
+package jenkins.plugins.decorator;
+
+import hudson.Extension;
+import hudson.model.PageDecorator;
+
+@Extension
+public class Decorator extends PageDecorator {
+}
